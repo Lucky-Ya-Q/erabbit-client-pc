50aaeb72b1d2fac058e9faf9241b46d492d6326f
刘元博 <1553592282@qq.com>
2022-05-06 21:00:21 +0800

    左侧分类-结构渲染

--- src/views/home/home-page.vue
+++ src/views/home/home-page.vue
@@ -1,9 +1,16 @@
 <template>
-  home-page
+  <div class="page-home">
+    <div class="home-entry">
+      <div class="container">
+        <!-- 左侧分类 -->
+        <home-category></home-category>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup>
-
+import HomeCategory from '@/views/home/components/home-category'
 </script>
 
 <style scoped lang="less">
